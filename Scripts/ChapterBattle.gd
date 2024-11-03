c049062aa9c3d0adf3bb634f812349352f723a6f
Jackachulian <jackachulian@gmail.com>
2024-11-03 11:17:31 -0600

    Attack/support range display

--- Scripts/ChapterBattle.gd
+++ Scripts/ChapterBattle.gd
@@ -6,6 +6,8 @@ extends Control
 @onready var range_display_grid: Node2D = $RangeDisplayGrid
 
 const MOVE_TILE_HIGHLIGHT: PackedScene = preload("res://Scenes/UI/MoveTileHighlight.tscn")
+const ATTACK_TILE_HIGHLIGHT = preload("res://Scenes/UI/AttackTileHighlight.tscn")
+const SUPPORT_TILE_HIGHLIGHT = preload("res://Scenes/UI/SupportTileHighlight.tscn")
 
 const RETRO_WIDTH: int = 19
 const RETRO_HEIGHT: int = 14
@@ -53,62 +55,135 @@ func move_cursor_to(coords: Vector2i):
 	hovered_unit = unit_grid.get_unit_at(cursor_coords)
 	cursor_moved.emit()
 
-# dictionaries to store the Vector2i coords as keys and dummy values
-var move_range: Dictionary
-var attack_range: Dictionary
+# dictionary to store the Vector2i coords as keys and dummy values
+var moveable_tiles: Dictionary
+var attackable_tiles: Dictionary
+
+var highest_attack_range: int
+var highest_support_range: int
+var total_attack_range_span: int
 
 func show_range(unit: PlacedUnit):	
 	for child in range_display_grid.get_children():
 		child.queue_free()
 		
-	move_range.clear()
-	attack_range.clear()
+	moveable_tiles.clear()
+	attackable_tiles.clear()
 	
 	if not unit:
 		return
 		
 	print("showing range of ", unit.unit.character.display_name, " from coords ", unit.coords)
 	
-	spread_range(unit.coords, unit, 0 if unit.moved else unit.unit.speed)
+	# Total range of abilities is move amount plus range of highest range attack
+	highest_attack_range = 0
+	highest_support_range = 0
+	for move in unit.unit.moves:
+		if move.move_type == "Attack":
+			highest_attack_range = max(highest_attack_range, move.range)
+		elif move.move_type == "Support":
+			highest_support_range = max(highest_support_range, move.range)
+			
+	total_attack_range_span = max(highest_attack_range, highest_support_range)
+	
+	spread_range(unit.coords, unit, unit.unit.speed)
 	
 	# can't move in place
-	move_range.erase(unit.coords)
+	moveable_tiles.erase(unit.coords)
 	
-	for moveable_coords in move_range.keys():
-		var highlight = MOVE_TILE_HIGHLIGHT.instantiate()
-		highlight.position = moveable_coords * TILE_SIZE
+	# TODO: only show attack in place if move is a support move
+	attackable_tiles.erase(unit.coords)
+	
+	
+	for coords in moveable_tiles.keys():
+		var highlight: Node2D = MOVE_TILE_HIGHLIGHT.instantiate()
+		highlight.position = coords * TILE_SIZE
 		range_display_grid.add_child(highlight)
+		
+	for coords in attackable_tiles.keys():
+		# Don't show the attack square if unit can move here, blue swuare should be shown instead
+		if coords in moveable_tiles.keys():
+			continue
+			
+		var tiles_left: int = attackable_tiles[coords]
+		var range_required: int = total_attack_range_span - tiles_left
+			
+		# Don't show the attack square if there is a unit here that is not the targe tof the current move.
+		# TODO: change logic based on whether or not move is a support move, or add green tiles for supportable allies?
+		var other_unit: PlacedUnit = unit_grid.get_unit_at(coords)
+		
+		# If there is indeed another unit here and they are on the same team, 
+		# check if they're in support range and draw a green square if so
+		if other_unit and other_unit.allied == unit.allied and highest_support_range >= range_required:
+			var highlight: Node2D = SUPPORT_TILE_HIGHLIGHT.instantiate()
+			highlight.position = coords * TILE_SIZE
+			range_display_grid.add_child(highlight)
+			
+		# Show attack range otherwise
+		elif highest_attack_range >= range_required:
+			var highlight: Node2D = ATTACK_TILE_HIGHLIGHT.instantiate()
+			highlight.position = coords * TILE_SIZE
+			range_display_grid.add_child(highlight)
+		
+		
 	
-# Mark as movable/attackable, and spreads movable and attackable range to adjacent tiles
+# Mark how many moves remaining at the coords, and spreads movable/attackable range to adjacent tiles
 func spread_range(coords: Vector2i, unit: PlacedUnit, move_remaining: int):
 	# Can't move on this tile if it is solid / there is a unit here
 	# (ignore this if this tile is where the unit currently is)
-	if is_tile_solid(coords) and not (unit.coords == coords):
+	if is_tile_impassable(coords) and not (unit.coords == coords):
 		return
 	
 	# If already marked as moveable, stop if unit can already reach this tile in fewer moves
-	if move_range.has(coords) and move_range[coords] > move_remaining:
+	if moveable_tiles.has(coords) and moveable_tiles[coords] > move_remaining:
 		return
 		
 	# Mark as a valid move location and mark the value as the moves it took to get here
-	print(coords, " reachable in ", move_remaining, " moves")
-	move_range[coords] = move_remaining
+	moveable_tiles[coords] = move_remaining
 	
-	# If this was the last tile, cant move anymore
+	# Spread attack range from this position that the unit can reach
+	spread_attack_range(coords, unit, total_attack_range_span)
+	
+	if move_remaining <= 0:
+		return
+	# Consume a move tile
 	move_remaining -= 1
-	if move_remaining < 0:
+		
+	# Try to spread move range to adjacent tiles
+	spread_range(coords + Vector2i.UP, unit, move_remaining)
+	spread_range(coords + Vector2i.RIGHT, unit, move_remaining)
+	spread_range(coords + Vector2i.DOWN, unit, move_remaining)
+	spread_range(coords + Vector2i.LEFT, unit, move_remaining)
+	
+func spread_attack_range(coords: Vector2i, unit: PlacedUnit, attack_range_remaining: int):
+	# stop if cursor is OOB
+	if not is_coords_in_bounds(coords):
+		return
+	
+	# If already marked as attackable, stop if unit can already reach this tile in fewer moves
+	if attackable_tiles.has(coords) and attackable_tiles[coords] > attack_range_remaining:
 		return
 		
-	spread_range(Vector2i(coords + Vector2i.UP), unit, int(move_remaining))
-	spread_range(Vector2i(coords + Vector2i.RIGHT), unit, int(move_remaining))
-	spread_range(Vector2i(coords + Vector2i.DOWN), unit, int(move_remaining))
-	spread_range(Vector2i(coords + Vector2i.LEFT), unit, int(move_remaining))
+	attackable_tiles[coords] = attack_range_remaining
+	
+	if attack_range_remaining <= 0:
+		return
+	attack_range_remaining -= 1
+	
+	spread_attack_range(coords + Vector2i.UP, unit, attack_range_remaining)
+	spread_attack_range(coords + Vector2i.RIGHT, unit, attack_range_remaining)
+	spread_attack_range(coords + Vector2i.DOWN, unit, attack_range_remaining)
+	spread_attack_range(coords + Vector2i.LEFT, unit, attack_range_remaining)
+	
+# Return true if this tile is either out of bounds or there is a solid tile here (for non retro mode).
+func is_tile_solid(coords: Vector2i):
+	return not is_coords_in_bounds(coords);
 	
 # True if the given tile is solid or there is a unit there (allied or enemy).
 # Units can only move on non-solid tiles
-func is_tile_solid(coords: Vector2i):
-	# If out of retro bounds return true
-	if not is_coords_in_bounds(coords):
+func is_tile_impassable(coords: Vector2i):
+	# If there is a solid tile here / coord is outside the map, return true
+	if is_tile_solid(coords):
 		return true
 		
 	# If there is a unit here return true
